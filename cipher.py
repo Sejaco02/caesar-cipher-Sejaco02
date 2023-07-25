c8fb8d5c809093aa27b91743a64c8f01b20746b6
Seth Jacobs <sejaco02@gmailo.com>
2023-07-25 14:05:38 -0400

    added comments to code file

--- cipher.py
+++ cipher.py
@@ -1,16 +1,17 @@
 # establish alphabet and cipher lists to be looped to create dictionary
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-cipher = ['f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e']
+cipher =   ['f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e']
 cipher_dictionary = {}
 
 # Populate cipher dictionary
 for letter in alphabet :
     cipher_dictionary[letter] = cipher[alphabet.index(letter)]
 
-# Request Input for word/sentence to cipher
+# Request Input for word/sentence to cipher and setup output field
 translate_me = input("Please enter statement to be encrypted? ")
 encryption = str()
 
+# run encryption and print output
 for char in translate_me:
     if char.isupper():
         encryption += ((cipher_dictionary.get(char.lower(),char))).upper()
